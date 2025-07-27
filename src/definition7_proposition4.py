08dbf37003df4f5b69eeea26928e9f818b27bffc
n-chikh <chikh.noureddine@enssea.edu.dz>
2025-07-27 10:31:08 +0100

    Add files via upload

---
+++ src/definition7_proposition4.py
@@ -0,0 +1,54 @@
+'''Implementation of Proposition 1'''
+
+
+'''The first function returns the (i-1,i)-transposition of a dominance list
+'''
+def i_transpose(i, dl):
+    '''Input: the transposition's position i
+              the dominance list to be stransposed 
+       Output: the (i-1,i)-transposition'''
+    if (i<=1 or i>len(dl) or dl[i-1]<=0):
+        print("This is not a (i-1, i)-transposition")
+    else:        
+        dlp = list(dl)
+        dlp[i-1]=dl[i-2]+1
+        dlp[i-2]=dl[i-1]-1
+        return dlp
+        
+        
+''' The second function check if two dominance lists are the 
+        (i-1,i)-transposes of each other
+'''
+def check_itranspose(dl,tdl):
+    '''Input: two dominance lists
+       Output: (True, the transposition's position), if dl and tdl are 
+               (i-1,i)-transposes of each-other;
+               or (False, 0), elsewhere.
+    '''
+    itranspose = False
+    if len(dl) == len(tdl):
+        i = 1
+        def tau_trans(i):
+            if (tdl[i] == dl[i-1] + 1) and (tdl[i-1] == dl[i] - 1) and (tdl[i+1:] == dl[i+1:]):
+                return True
+            else: return False
+            
+        while (itranspose == False) and (i < len(dl)):
+            if tau_trans(i) == True:
+                itranspose = True
+            else:
+                i += 1
+    return itranspose, (None if (itranspose == False) else i)
+    
+
+'''Module execution
+   (generate an image of the corresponding N-construction)
+'''
+if __name__ == "__main__":
+    import sys
+    #print("Transposition's position, then the dominance list")
+    inp = sys.argv[1]
+    inp2 = int(sys.argv[2])
+    form_inp = list(map(int, inp.strip('[]').split(',')))
+    td = i_transpose(inp2, form_inp)
+    if td != None: print("τ(d) = {}".format(td))
